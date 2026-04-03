dd09d7c53604a89a803cae4487ba5ab6430adb6e
OzirvsDev <donluka94@gmail.com>
2026-04-03 22:00:21 +0200

    correction port

--- back-azure/server.js
+++ back-azure/server.js
@@ -35,5 +35,5 @@ app.post('/api/addUser', async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Serveur démarré sur http://localhost:${PORT}`));
